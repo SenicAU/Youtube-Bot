90c66b8d13c00f21a7d540453fee499ce3050e10
SenicAU <>
2021-10-17 13:34:26 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const { token } = require("./config.json");
+const discord = require ("discord.js");
+const client = new discord.Client({
+  disableEveryone: true 
+});
+
+client.commands = new discord.Collection();
+client.aliases = new discord.Collection();
+
+["command", "events"].forEach(handler => {
+  require(`./handlers/${handler}`)(client);
+});
+
+
+client.login(process.env.token)
+const mySecret = process.env['token']
